6452b067987eeba855d2bbc8da7e5764d0661384
kobe-victor <victor.goulart@kobe.io>
2020-02-01 15:13:30 -0300

    Migrate libraries to androidx Added MVVM Removed logic from HomeActivity and added logic to HomeViewModel

---
+++ app/src/main/java/com/arctouch/codechallenge/viewmodel/HomeViewModel.kt
@@ -0,0 +1,29 @@
+package com.arctouch.codechallenge.viewmodel
+
+import android.view.View
+import androidx.lifecycle.MutableLiveData
+import com.arctouch.codechallenge.api.TmdbApi
+import com.arctouch.codechallenge.data.Cache
+import com.arctouch.codechallenge.model.Movie
+import com.arctouch.codechallenge.util.NetworkUtils
+import com.arctouch.codechallenge.view.adapter.HomeAdapter
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.home_activity.*
+
+class HomeViewModel : BaseViewModel() {
+
+    var moviesLiveData = MutableLiveData<List<Movie>>()
+
+    fun getUpcomingMovies() {
+        NetworkUtils.getInstance().upcomingMovies(TmdbApi.API_KEY, TmdbApi.DEFAULT_LANGUAGE, 1, TmdbApi.DEFAULT_REGION)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    moviesLiveData.value = it.results.map { movie ->
+                        movie.copy(genres = Cache.genres.filter { movie.genreIds?.contains(it.id) == true })
+                    }
+                }
+    }
+
+}
